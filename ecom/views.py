2b48053eb59ec9a3af53021b5542b31ded4834db
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-11-03 15:50:58 +0530

    Commit:6 (paypal)

--- ecom/views.py
+++ ecom/views.py
@@ -1,3 +1,26 @@
+import uuid
+
 from django.shortcuts import render
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm 
+from django.conf import settings
 
 # Create your views here.
+
+def home(request):
+    host = request.get_host()
+    paypal_dict = {
+        'business':settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '20.00',
+        'item_name':'Product 1',
+        'invoice': str(uuid.uuid4()),
+        'currency_code': 'USD',
+        'notify_url': f'http://{host}{reverse("paypal-ipn")}',
+        'return_url': f'http://{host}{reverse("paypal-reverse")}',
+        'cancel_return': f'http://{host}{reverse("paypal-cancel")}',
+    }
+    form = PayPalPaymentsForm(initial= paypal_dict)
+    context = {
+        'form':form
+    }
+    return render(request, 'home.html', context)
